d47de7b1b4a429ed3d4819f3ef5f1ab7dd749f78
hwancom <hwancom@gmail.com>
2019-05-13 02:17:19 +0900

    studying

--- app/src/main/java/com/hwancom/study_intent/MainActivity.java
+++ app/src/main/java/com/hwancom/study_intent/MainActivity.java
@@ -6,28 +6,29 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int REQUEST_CODE = 1000;
-    private EditText mIdText;
-    private EditText mPasswordText;
+    private EditText mId;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mIdText = findViewById(R.id.idEditText);
-        mPasswordText = findViewById(R.id.passEditText);
+        mId = findViewById(R.id.idEditText);
+        mPassword = findViewById(R.id.passEditText);
         findViewById(R.id.loginButton).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, MemberActivity.class);
-        intent.putExtra("id", mIdText.getText().toString());
-        intent.putExtra("password", mPasswordText.getText().toString());
+        intent.putExtra("id", mId.getText().toString());
+        intent.putExtra("pwd", mPassword.getText().toString());
 
         startActivityForResult(intent, REQUEST_CODE);
         /** REQUEST_CODE는 Find Action을 통해 상수로 선언한다 */
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            String text = data.getStringExtra("result");
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/hwancom/study_intent/MemberActivity.java
+++ app/src/main/java/com/hwancom/study_intent/MemberActivity.java
@@ -1,13 +1,48 @@
 package com.hwancom.study_intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MemberActivity extends AppCompatActivity {
+public class MemberActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member);
+
+        if (getIntent() != null) {
+            String id = getIntent().getStringExtra("id");
+            String password = getIntent().getStringExtra("pwd");
+
+            Toast.makeText(this, "ID : " + id + ", Password : " + password, Toast.LENGTH_SHORT).show();
+
+            findViewById(R.id.customerButton).setOnClickListener(this);
+            findViewById(R.id.salesButton).setOnClickListener(this);
+            findViewById(R.id.goodsButton).setOnClickListener(this);
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent();
+
+        switch (view.getId()) {
+            case R.id.customerButton :
+                intent.putExtra("result", "고객 관리");
+                break;
+            case R.id.salesButton :
+                intent.putExtra("result", "매출 관리");
+                break;
+            case R.id.goodsButton :
+                intent.putExtra("result", "상품 관리");
+        }
+
+        setResult(RESULT_OK, intent);
+    finish();
     }
 }
