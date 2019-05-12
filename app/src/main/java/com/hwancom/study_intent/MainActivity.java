3c8905e7e22fec74f73923f9ae0ea35d1c8925de
hwancom <hwancom@gmail.com>
2019-05-12 23:29:22 +0900

    ...

--- app/src/main/java/com/hwancom/study_intent/MainActivity.java
+++ app/src/main/java/com/hwancom/study_intent/MainActivity.java
@@ -1,13 +1,42 @@
 package com.hwancom.study_intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int REQUEST_CODE = 1000;
+    private EditText mIdText;
+    private EditText mPasswordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIdText = findViewById(R.id.idEditText);
+        mPasswordText = findViewById(R.id.passEditText);
+        findViewById(R.id.loginButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MemberActivity.class);
+        intent.putExtra("id", mIdText.getText().toString());
+        intent.putExtra("password", mPasswordText.getText().toString());
+
+        startActivityForResult(intent, REQUEST_CODE);
+        /** REQUEST_CODE는 Find Action을 통해 상수로 선언한다 */
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
     }
 }
